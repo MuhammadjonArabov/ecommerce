be9397ba2d8b1e7c76f7910f91353d2808af01e1
MuhammadjonArabov <arabovmuhammadjon1301@gmail.com>
2024-06-28 06:06:27 +0500

    product serializers

--- api/product/serializer.py
+++ api/product/serializer.py
@@ -3,3 +3,59 @@ from rest_framework import serializers
 from config.settings.base import env
 from common.product.models import Product, ProductImage
 
+
+class ProductCreateSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'guid', 'title', 'price', 'description', 'quantity', 'category']
+
+
+class ProductListSerializers(serializers.ModelSerializer):
+    productImage = serializers.SerializerMethodField()
+
+
+
+class ProductShortSerializers(serializers.ModelSerializer):
+    warehouse_product_quantity = serializers.SerializerMethodField()
+    total_quantity = serializers.SerializerMethodField()
+
+    def get_warehouse_product_quantity(self, obj):
+        warehouses = self.context['warehouses']
+        data = {}
+        for warehouse in warehouses:
+            matching_products = [k for k in obj.productWarehouseProduct.all() if k.warehouse.id == warehouse.id]
+            data[warehouse.id] = sum(k.quantity for k in matching_products) if matching_products else 0
+        return data
+
+    def get_total_quantity(self, obj):
+        if hasattr(obj, 'total_quantity'):
+            return obj.total_quantity or 0
+        return 0
+
+    class Meta:
+        model = Product
+        fields = ['id', 'guid', 'title', 'total_quantity', 'warehouse_product_quantity']
+
+
+class ProductImageListSerializers(serializers.ModelSerializer):
+    photo = serializers.SerializerMethodField()
+
+    def get_photo(self, product):
+        if product.photo:
+            return env.str('BASE_URL') + product.photo.url
+        return ''
+
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'guid', 'photo', 'isMain']
+
+
+class ProductImageCreateSerializers(serializers.ModelSerializer):
+    photo = Base64ImageField(required=True)
+    id = serializers.IntegerField(read_only=True)
+    guid = serializers.CharField(read_only=True)
+    isMain = serializers.BooleanField(required=True)
+
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'guid', 'photo', 'isMain', 'product']
